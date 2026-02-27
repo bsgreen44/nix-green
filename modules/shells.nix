433ff5930e60f9482fcb060fdbfc79c8265f2935
bsgreen44 <ybsgreen44@proton.me>
2026-02-27 14:58:09 -0700

    Changed to unstable branch. Changed shell to zsh. Added tsui.

--- modules/shells.nix
+++ modules/shells.nix
@@ -1,26 +1,38 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  programs.bash = {
+  home.shellAliases = {
+  cat = "bat";
+  };
+
+  # Uncomment to enable bash
+  #programs.bash = {
+  #  enable = true;
+  #  initExtra = ''
+  #    eval "$(starship init bash)"
+  #  '';
+  #};
+
+  programs.zsh = {
     enable = true;
-    initExtra = ''
-      eval "$(starship init bash)"
+    plugins = [
+      { name = "zsh-autosuggestions";
+        src = "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions";
+      }
+      { name = "zsh-syntax-highlighting";
+        src = "${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting";
+      }
+    ];
+    initContent = ''
+      eval "$(starship init zsh)"
     '';
   };
 
-  # Uncomment to enable zsh
-  # programs.zsh = {
-  #   enable = true;
-  #   initExtra = ''
-  #     eval "$(starship init zsh)"
-  #   '';
-  # };
-
   # Uncomment to enable fish
-  # programs.fish = {
-  #   enable = true;
-  #   interactiveShellInit = ''
-  #     starship init fish | source
-  #   '';
-  # };
+  #programs.fish = {
+  #  enable = true;
+  #  interactiveShellInit = ''
+  #    starship init fish | source
+  #  '';
+  #};
 }
