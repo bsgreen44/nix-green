53167ead749a68c44519d4009daa6c18f6861435
bsgreen44 <ybsgreen44@proton.me>
2026-03-16 11:11:58 -0600

    Fixed screensaver bug that resets hypridle timeout.
    This caused a new screensaver session to open after each iteration
    causing an infinite loop.

--- modules/hypridle.nix
+++ modules/hypridle.nix
@@ -18,22 +18,24 @@
           on-resume = "brightnessctl -r"; # Restore brightness
         }
         {
-          timeout = 180; # 3 minutes turn on screensaver. After 5 minutes lock the scren   
-          on-timeout = ''ghostty --title=full --font-size=29 -e sh -c "( sleep 120 && loginctl lock-session && pkill -f 'ghostty.*title=full' ) & TIMER_PID=\$!; ( while true; do cols=\$(tput cols); rows=\$(tput lines); COLS=\$cols python3 \$HOME/.local/share/center_logo.py | tte --canvas-width \$cols --canvas-height \$rows --random-effect; done ) & LOOP_PID=\$!; read -n 1 -s; kill \$LOOP_PID 2>/dev/null; kill \$TIMER_PID 2>/dev/null; pkill -f 'ghostty.*title=full'"'';
+          # 3 minutes turn on screensaver.
+          # After 5 minutes lock the screen and turn display off. After 20 minutes suspend
+          timeout = 180;
+          on-timeout = ''ghostty --title=full --font-size=25 -e sh -c "( sleep 120 && loginctl lock-session && pkill -f 'ghostty.*title=full' && sleep 2 && hyprctl dispatch dpms off && sleep 1080 && systemctl suspend ) & TIMER_PID=\$!; ( while true; do cols=\$(tput cols); rows=\$(tput lines); COLS=\$cols python3 \$HOME/.local/share/center_logo.py | tte --canvas-width \$cols --canvas-height \$rows --random-effect; done ) & LOOP_PID=\$!; read -n 1 -s; kill \$LOOP_PID 2>/dev/null; kill \$TIMER_PID 2>/dev/null; pkill -f 'ghostty.*title=full'"'';
         }
         #{
         #  timeout = 180; # 3 minutes
         #  on-timeout = "loginctl lock-session"; # Lock screen
         #}
-        {
-          timeout = 330; # 5.5 minutes
-          on-timeout = "hyprctl dispatch dpms off"; # Screen off
-          on-resume = "hyprctl dispatch dpms on"; # Screen on
-        }
-        {
-          timeout = 1800; # 30 minutes
-          on-timeout = "systemctl suspend"; # Suspend PC
-        }
+        #{
+        #  timeout = 330; # 5.5 minutes
+        #  on-timeout = "hyprctl dispatch dpms off"; # Screen off
+        #  on-resume = "hyprctl dispatch dpms on"; # Screen on
+        #}
+        #{
+        #  timeout = 1800; # 30 minutes
+        #  on-timeout = "systemctl suspend"; # Suspend PC
+        #}
       ];
     };
   };
